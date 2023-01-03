be7bdb77ce5fe215e92da82728427868b808e134
DharmaWarrior <97218268+DharmaWarrior@users.noreply.github.com>
2023-01-03 16:50:04 +0530

    First Proto

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from './components/Card'
+import data from './data'
+
+function App() {
+
+  const cards = data.map((item) => {
+    return (
+      <Card
+       key = {item.id} 
+       cover = {item.cover}
+       rating = {item.stats.rating}
+       reviewCount = {item.stats.reviewCount}
+       location = {item.location}
+       title = {item.title}
+       price = {item.price}
+        />
+    )
+  })
+  
+  
+  return (
+      <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
+      </div>
+  )
+}
+
+export default App
